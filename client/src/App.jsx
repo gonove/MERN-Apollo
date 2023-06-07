2b84d7fca69079f0469b02a2fa19e457c8a2b3da
gonove <gonzalezovelar97@gmail.com>
2023-06-06 23:27:19 -0300

    feat:config de frontend, add pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React from 'react';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Projects } from './pages/Projects';
+import { ProjectDetails } from './pages/ProjectDetails';
+import { Dashboard } from './pages/Dashboard';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql ',
+  cache: new InMemoryCache(),
+});
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Dashboard />,
+  },
+  {
+    path: '/projects',
+    element: <Projects />,
+  },
+  {
+    path: '/projects/:id',
+    element: <ProjectDetails />,
+  },
+]);
 
 export const App = () => {
   return (
-    <>
-      <h1>Hello world</h1>
-    </>
-  )
-}
+    <ApolloProvider client={client}>
+      <RouterProvider router={router} />
+    </ApolloProvider>
+  );
+};
