4d1c77fdd76b46dc007e6aff5a9c18faf24fcb27
gonove <gonzalezovelar97@gmail.com>
2023-06-07 00:55:39 -0300

    feat:crear proyectos usando mutations, configs y parametros necesarios

--- client/src/components/ProjectForm.jsx
+++ client/src/components/ProjectForm.jsx
@@ -1,7 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react';
+
+import { useMutation } from '@apollo/client';
+import { CREATE_PROJECT, GET_PROJECTS } from '../graphql/projects';
 
 export const ProjectForm = () => {
+  const [project, setProject] = useState({
+    title: '',
+    description: '',
+  });
+
+  const [createProject, { loading, error }] = useMutation(CREATE_PROJECT, {
+    // para refrescar la pantalla
+    refetchQueries: [{ query: GET_PROJECTS }, 'GetProjects'],
+  });
+
+  const handleChange = (e) => {
+    setProject({
+      ...project,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    createProject({
+      variables: {
+        name: project.name,
+        description: project.description,
+      },
+    });
+  };
+
   return (
-    <div>ProjectForm</div>
-  )
-}
+    <form onSubmit={handleSubmit}>
+      {error && <p> Damn ⛔️ {error.message} </p>}
+
+      <input
+        type="text"
+        name="name"
+        placeholder="Write a title"
+        onChange={handleChange}
+      />
+      <textarea
+        name="description"
+        rows="3"
+        placeholder="Write a description"
+        onChange={handleChange}
+      ></textarea>
+
+      <button
+        type="submit"
+        disabled={!project.name || !project.description || loading}
+      >
+        Submit
+      </button>
+    </form>
+  );
+};
